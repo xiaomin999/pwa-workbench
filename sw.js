7fec92c68c68fb5cc06b5341d6a0ce4f29e0f747
xiaomin999 <107472323+xiaomin999@users.noreply.github.com>
2026-07-23 14:50:14 +0800

    sync sw.js via sync_site.py

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'wb-app-v13';
+const CACHE = 'wb-app-v15';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', function (e) {
   e.waitUntil(
